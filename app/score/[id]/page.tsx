950617a69d80f30ffcc626abb479a3261fa81f4b
anirudhramoo <anirudh@getpassage.co>
2023-07-27 10:39:20 -0400

    make share link work

--- app/score/[id]/page.tsx
+++ app/score/[id]/page.tsx
@@ -1,7 +1,7 @@
 import {Score} from "@/app/score/[id]/Score";
 
 const Page=({ params }: { params: { id: string } })=>(
-    <div className={"w-full h-full grid place-items-center"}>
+    <div className={"w-screen h-screen grid place-items-center"}>
         <Score id={params.id}/>
     </div>
 )
